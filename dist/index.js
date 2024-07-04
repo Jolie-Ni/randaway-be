b288c02df59dd1b5119269ad3d1eb65e4b13e830
Jolie Ni <ninan980805@gmail.com>
2024-07-03 22:46:34 -0400

    add a new endpoint

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,10 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+// connect to aws dynamoDB and display a list of db entries
+app.get('/locations', (req, res) => {
+    res.send('this endpoint will return a list of locations parsed from instagram posts');
+});
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+
+// connect to aws dynamoDB and display a list of db entries
+app.get('/locations', (req: Request, res: Response) => {
+    res.send('this endpoint will return a list of locations parsed from instagram posts');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
